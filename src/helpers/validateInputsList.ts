018070577793468dbb3da5bcfff69b8f2a6d8477
Valentin Pimenov <valentin-7696@yandex.com>
2022-12-03 17:47:34 +0300

    dev

--- src/helpers/validateInputsList.ts
+++ src/helpers/validateInputsList.ts
@@ -1,15 +1,13 @@
 export default function validateInputsList(inputs: Record<string, any>) {
-    const data = {} as Record<string, unknown>
+    let data = {} as Record<string, unknown>
     let check = true
-    for (const [key, value] of Object.entries(inputs)) {
 
-        if (value.constructor.name === "Input") {
-            const input = value.inputValidate()
-            if (input || input === null) {
-                data[key] = input
-            } else {
-                check = false
-            }
+    for (const [key, value] of Object.entries(inputs)) {
+        const input = value.inputValidate()
+        if (input || input === null) {
+            data[key] = input
+        } else {
+            check = false
         }
     }
     return check ? data : check
