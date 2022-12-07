c2e7068959d7db3be35820fab9319c2ee06ecd94
Valentin Pimenov <valentin-7696@yandex.com>
2022-12-07 22:01:15 +0300

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,69 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 
 const isProd = process.env.NODE_ENV == 'production'
 const isDev = !isProd
 
-const config = {
-    entry: './src/app.ts',
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
     output: {
         filename: isProd ? "bundle.[hash].js" : "bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
-        open: true,
-        host: 'localhost',
-        compress: true,
-        port: 3000
+        hot: isDev,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            handlebars: 'handlebars/dist/handlebars.js',
+        }
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: "./src/index.html",
             minify: {
                 removeComments: isProd,
                 collapseWhitespace: isProd
             },
             inject: 'body'
         }),
-
         new MiniCssExtractPlugin({
             filename: isProd ? 'bundle.[hash].css' : 'bundle.css',
-        })
+        }),
     ],
     module: {
         rules: [
             {
-                test: /\.(ts)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.css$/i,
-                loader: "css-loader",
-                options: {
-                    mode: "local",
-                    auto: true,
-                    exportGlobals: true,
-                    localIdentName: "[path][name]__[local]--[hash:base64:5]",
-                    localIdentContext: path.resolve(__dirname, "src"),
-                    localIdentHashSalt: "my-custom-hash",
-                    namedExport: true,
-                    exportLocalsConvention: "camelCase",
-                    exportOnlyLocals: false,
-                },
-            },
-            {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                        }
-                    },
-                    // Compiles Sass to CSS
-                    'sass-loader'
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
                 ],
+            },
+            {
+                test: /\.ts$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/,
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.sass', '...'],
-        alias: {
-            'handlebars': 'handlebars/dist/handlebars.js'
-        }
-    },
-}
+        ]
+    }
+};
